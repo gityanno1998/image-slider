6c6bc7877d892f01809a4795f78f41e633fcf36d
gityanno1998 <yannagre1997@gmail.com>
2023-11-16 23:35:55 +0300

    app and style changes for the slider

--- app.js
+++ app.js
@@ -16,9 +16,29 @@ next.onclick = function () {
   reloadSlider();
 };
 
+prev.onclick = function () {
+  if (active - 1 < 0) {
+    active = lengthItems;
+  } else {
+    active = active - 1;
+  }
+  reloadSlider();
+};
+
+let refreshSlider = setInterval(()=> {next.click}, 3000);
+
 function reloadSlider() {
   let checkLeft = items[active].offsetLeft;
   list.style.left = -checkLeft + "px";
 
   let lastActiveDot = document.querySelector(".slider .dots li.active");
+  lastActiveDot.classList.remove("active");
+  dots[active].classList.add("active");
+}
+
+dots.forEach(li, key) => {
+  li.addEventListener('click', function(){
+    active = key;
+    reloadSlider();
+  })
 }
